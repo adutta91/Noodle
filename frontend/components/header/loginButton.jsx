c370785f832748d183f506053bc7202512b3d4f2
Arjun <arjundutta91@gmail.com>
2016-07-15 16:02:35 -0700

    finalize login modal

--- frontend/components/header/loginButton.jsx
+++ frontend/components/header/loginButton.jsx
@@ -3,21 +3,57 @@ var React = require('react');
 // FLUX
 var SessionUtil = require('../../utils/sessionUtil');
 
+// COMPONENTS
+var Modal = require('boron/OutlineModal');
+var LoginForm = require('./loginForm');
+
+// MODAL STYLES
+var modalStyle = {
+  transform : 'inherit',
+  width: '300px',
+  transform: 'translate(-50%, -50%)',
+  border: '1px solid black',
+  borderRadius: '3px'
+};
+
+var backdropStyle = {
+  backgroundColor: 'rgba(0, 0, 0, 0.2)'
+};
+
+var contentStyle = {
+  height: '100%',
+  padding: '20px'
+};
+
 var LoginButton = React.createClass({
 
-  login: function(event) {
-    event.preventDefault();
-    SessionUtil.loginUser({
-      user: {
-        username: 'adutta',
-        password: 'password'
-      }
+  getInitialState: function() {
+    return ({
+      open: false
     });
+  },
+
+  showModal: function() {
+    this.refs.modal.show();
+    this.setState({ open: true });
+  },
+
+  hideModal: function() {
+    this.refs.modal.hide();
+    this.setState({ open: false });
   },
 
   render: function() {
     return (
-      <div className="button" onClick={this.login}>Login</div>
+      <div>
+        <div className="button" onClick={this.showModal}>Login</div>
+        <Modal ref="modal"
+              contentStyle={contentStyle}
+              modalStyle={modalStyle}
+              backdropStyle={backdropStyle}>
+          <LoginForm modalCallback={this.hideModal}/>
+        </Modal>
+      </div>
     )
   }
 })
